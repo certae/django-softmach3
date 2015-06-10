bbe9bbfb982fe6bcd4edcf52038489ef67880e99
dario <dariogomezt@gmail.com>
2015-06-10 18:08:36 -0400

    Fixtures

--- dumpdata.sh
+++ dumpdata.sh
@@ -1,1 +1,2 @@
-python manage.py dumpdata --natural-foreign --natural-primary --indent 2 protoLib > protolib.json 
+python manage.py dumpdata --indent 2 auth > fixtures/auth.json 
+python manage.py dumpdata --indent 2 protoLib > fixtures/protolib.json 
