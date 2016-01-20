a52e49686cf8f1b4a09e58feff322bb9cd841810
dario <dariogomezt@gmail.com>
2016-01-20 15:06:23 -0500

    Delete js.tmp

--- protoExt/static/tmp/initcomponent.js
+++
@@ -1,9 +0,0 @@
-Ext.define('DynamicButtonText', {
-    extend : 'Ext.button.Button',
-
-    initComponent : function(){
-        this.text = new Date();
-        this.renderTo = Ext.getBody();
-        this.callParent();
-    }
-});
--- protoExt/static/tmp/viewport/DashViewPMovel.js
+++
@@ -1,5 +0,0 @@
-(Ext.cmd.derive("Admin.view.main.ViewportModel", Ext.app.ViewModel, {
-    data : {
-        currentView : null
-    }
-}, 0, 0, 0, 0, [ "viewmodel.mainviewport" ], 0, [ Admin.view.main, "ViewportModel" ], 0));
