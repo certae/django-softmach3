f28d9ba1597e4bc1765a3b9f9302aba7e98410ae
dario <dariogomezt@gmail.com>
2016-03-10 15:47:43 -0500

    Req Mathew

---
+++ protoBase/__init__.py
@@ -0,0 +1,22 @@
+"""
+TODO :  Req Mathew
+
+Auto user Creation  
+django-registration 2.0
+https://github.com/ubernostrum/django-registration/tree/2.0.4/registration
+
+
+Una alternativa es agregar un boton en la forma,  
+* crear el usuario inactivo 
+* enviar un correo al administrador 
+	* Crear un perfil por default para la creacion de usuarios 
+	* solicitar un perfil por defecto combo  
+	* solicitar un perfil por defecto, un a validar por el admin
+* procedimiento de activacion 
+	* enviar correo de bienvenida al uusario   
+
+QBE 
+* Buscar en softmachine 
+
+
+"""
