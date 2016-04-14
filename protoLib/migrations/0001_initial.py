a71ae1ae9797b9d90a2504ad461253c27c787263
dario <dariogomezt@gmail.com>
2016-04-14 14:07:34 -0400

    DB nEW

--- protoLib/migrations/0001_initial.py
+++ protoLib/migrations/0001_initial.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import jsonfield2.fields
-import uuid
 from django.conf import settings
+import uuid
+import jsonfield2.fields
 
 
 class Migration(migrations.Migration):
@@ -18,53 +18,53 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ContextEntity',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('smNaturalCode', models.CharField(null=True, editable=False, max_length=50, blank=True)),
-                ('smRegStatus', models.CharField(null=True, editable=False, max_length=50, blank=True)),
-                ('smWflowStatus', models.CharField(null=True, editable=False, max_length=50, blank=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('smNaturalCode', models.CharField(null=True, blank=True, editable=False, max_length=50)),
+                ('smRegStatus', models.CharField(null=True, blank=True, editable=False, max_length=50)),
+                ('smWflowStatus', models.CharField(null=True, blank=True, editable=False, max_length=50)),
                 ('smCreatedOn', models.DateTimeField(null=True, auto_now_add=True)),
                 ('smModifiedOn', models.DateTimeField(null=True, auto_now=True)),
-                ('smUUID', models.UUIDField(default=uuid.uuid4, editable=False)),
-                ('propName', models.CharField(null=True, default='', max_length=200, blank=True)),
+                ('smUUID', models.UUIDField(editable=False, default=uuid.uuid4)),
+                ('propName', models.CharField(blank=True, null=True, max_length=200, default='')),
                 ('active', models.BooleanField(default=True)),
             ],
         ),
         migrations.CreateModel(
             name='ContextUser',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('smNaturalCode', models.CharField(null=True, editable=False, max_length=50, blank=True)),
-                ('smRegStatus', models.CharField(null=True, editable=False, max_length=50, blank=True)),
-                ('smWflowStatus', models.CharField(null=True, editable=False, max_length=50, blank=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('smNaturalCode', models.CharField(null=True, blank=True, editable=False, max_length=50)),
+                ('smRegStatus', models.CharField(null=True, blank=True, editable=False, max_length=50)),
+                ('smWflowStatus', models.CharField(null=True, blank=True, editable=False, max_length=50)),
                 ('smCreatedOn', models.DateTimeField(null=True, auto_now_add=True)),
                 ('smModifiedOn', models.DateTimeField(null=True, auto_now=True)),
-                ('smUUID', models.UUIDField(default=uuid.uuid4, editable=False)),
-                ('propValue', models.CharField(null=True, max_length=200, blank=True)),
-                ('description', models.TextField(null=True, blank=True)),
+                ('smUUID', models.UUIDField(editable=False, default=uuid.uuid4)),
+                ('propValue', models.CharField(blank=True, null=True, max_length=200)),
+                ('description', models.TextField(blank=True, null=True)),
                 ('active', models.BooleanField(default=True)),
             ],
         ),
         migrations.CreateModel(
             name='ContextVar',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('smNaturalCode', models.CharField(null=True, editable=False, max_length=50, blank=True)),
-                ('smRegStatus', models.CharField(null=True, editable=False, max_length=50, blank=True)),
-                ('smWflowStatus', models.CharField(null=True, editable=False, max_length=50, blank=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('smNaturalCode', models.CharField(null=True, blank=True, editable=False, max_length=50)),
+                ('smRegStatus', models.CharField(null=True, blank=True, editable=False, max_length=50)),
+                ('smWflowStatus', models.CharField(null=True, blank=True, editable=False, max_length=50)),
                 ('smCreatedOn', models.DateTimeField(null=True, auto_now_add=True)),
                 ('smModifiedOn', models.DateTimeField(null=True, auto_now=True)),
-                ('smUUID', models.UUIDField(default=uuid.uuid4, editable=False)),
-                ('propName', models.CharField(default='id', max_length=500)),
-                ('description', models.TextField(null=True, blank=True)),
+                ('smUUID', models.UUIDField(editable=False, default=uuid.uuid4)),
+                ('propName', models.CharField(max_length=500, default='id')),
+                ('description', models.TextField(blank=True, null=True)),
                 ('modelCType', models.ForeignKey(to='contenttypes.ContentType')),
-                ('smCreatedBy', models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False)),
-                ('smModifiedBy', models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False)),
+                ('smCreatedBy', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+')),
+                ('smModifiedBy', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+')),
             ],
         ),
         migrations.CreateModel(
             name='EntityMap',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('entityConfig', jsonfield2.fields.JSONField(default={})),
                 ('entityBase', models.OneToOneField(to='contenttypes.ContentType')),
             ],
@@ -72,51 +72,51 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Logger',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('smCreatedOn', models.DateTimeField(null=True, auto_now=True)),
-                ('logType', models.CharField(default='INF', max_length=10)),
-                ('logObject', models.CharField(null=True, max_length=250, blank=True)),
-                ('logNotes', models.CharField(null=True, max_length=250, blank=True)),
-                ('logInfo', models.TextField(null=True, blank=True)),
-                ('logKey', models.CharField(choices=[('INF', 'INFO'), ('WAR', 'WARNING'), ('ERR', 'ERROR'), ('INS', 'INSERT'), ('UPD', 'UPDATE'), ('DEL', 'DELETE')], default='', max_length=5)),
-                ('smCreatedBy', models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False)),
+                ('logType', models.CharField(max_length=10, default='INF')),
+                ('logObject', models.CharField(blank=True, null=True, max_length=250)),
+                ('logNotes', models.CharField(blank=True, null=True, max_length=250)),
+                ('logInfo', models.TextField(blank=True, null=True)),
+                ('logKey', models.CharField(max_length=5, default='', choices=[('INF', 'INFO'), ('WAR', 'WARNING'), ('ERR', 'ERROR'), ('INS', 'INSERT'), ('UPD', 'UPDATE'), ('DEL', 'DELETE')])),
+                ('smCreatedBy', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+')),
             ],
         ),
         migrations.CreateModel(
             name='TeamHierarchy',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('code', models.CharField(unique=True, max_length=200)),
-                ('description', models.TextField(null=True, verbose_name='Descriptions', blank=True)),
-                ('site', models.IntegerField(null=True, blank=True)),
-                ('parentNode', models.ForeignKey(to='protoLib.TeamHierarchy', related_name='downHierachy', null=True, blank=True)),
+                ('description', models.TextField(blank=True, verbose_name='Descriptions', null=True)),
+                ('site', models.IntegerField(blank=True, null=True)),
+                ('parentNode', models.ForeignKey(blank=True, to='protoLib.TeamHierarchy', related_name='downHierachy', null=True)),
             ],
         ),
         migrations.CreateModel(
             name='UserProfile',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('language', models.CharField(null=True, max_length=500, blank=True)),
-                ('userTree', models.CharField(null=True, max_length=500, blank=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('language', models.CharField(blank=True, null=True, max_length=500)),
+                ('userTree', models.CharField(blank=True, null=True, max_length=500)),
                 ('userConfig', jsonfield2.fields.JSONField(default={})),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
-                ('userTeam', models.ForeignKey(to='protoLib.TeamHierarchy', related_name='userTeam', null=True, blank=True)),
+                ('userTeam', models.ForeignKey(blank=True, to='protoLib.TeamHierarchy', related_name='userTeam', null=True)),
             ],
         ),
         migrations.AddField(
             model_name='logger',
             name='smOwningTeam',
-            field=models.ForeignKey(to='protoLib.TeamHierarchy', blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to='protoLib.TeamHierarchy', editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextvar',
             name='smOwningTeam',
-            field=models.ForeignKey(to='protoLib.TeamHierarchy', blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to='protoLib.TeamHierarchy', editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextvar',
             name='smOwningUser',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextuser',
@@ -126,22 +126,22 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='contextuser',
             name='smCreatedBy',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextuser',
             name='smModifiedBy',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextuser',
             name='smOwningTeam',
-            field=models.ForeignKey(to='protoLib.TeamHierarchy', blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to='protoLib.TeamHierarchy', editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextuser',
             name='smOwningUser',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextentity',
@@ -156,22 +156,22 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='contextentity',
             name='smCreatedBy',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextentity',
             name='smModifiedBy',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextentity',
             name='smOwningTeam',
-            field=models.ForeignKey(to='protoLib.TeamHierarchy', blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to='protoLib.TeamHierarchy', editable=False, null=True, related_name='+'),
         ),
         migrations.AddField(
             model_name='contextentity',
             name='smOwningUser',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, related_name='+', null=True, editable=False),
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, editable=False, null=True, related_name='+'),
         ),
         migrations.AlterUniqueTogether(
             name='contextvar',
