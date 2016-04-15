9ae3d41dddcd58558dee8481d743be4e6ef8a22d
dario <dariogomezt@gmail.com>
2016-04-15 11:29:26 -0400

    AutoTreeZoom for treeGrid CopyRow

--- protoExt/static/js/globals/Tools.js
+++ protoExt/static/js/globals/Tools.js
@@ -516,80 +516,26 @@ _SM.Product  = function (list) {
     }
 };
 
+_SM.getAutoTreeGridZoom = function(){
 
-// DIAGRAM DESIGNER
-var dbModel = {
-    shape: {},
-	locator: {}
+        var zoomCode = this.viewCode + '.zoom'
+        var myZoomMeta = _SM._cllPCI[this.zoomCode]
+
+        // if found, retunr 
+        if ( myZoomMeta ) {
+            return zoomCode; 
+        }
+
+        // Build zoomMeta 
+        myZoomMeta = _SM.clone( this.myMeta );
+
+        myZoomMeta = Ext.apply({
+            'viewCode' : this.zoomCode, 
+            'pciStyle' : 'grid'
+        }) 
+
+        _SM._cllPCI[ zoomCode]  = myZoomMeta ; 
+
+        return zoomCode 
 };
-var jsonDocument = [];
 
-
-function loadJsFilesSequentially(scriptsCollection, startIndex, librariesLoadedCallback) {
-    if (scriptsCollection[startIndex]) {
-    	if (filesadded.indexOf("[" + scriptsCollection[startIndex] + "]") === -1){
-	        var fileref = document.createElement('script');
-	        fileref.setAttribute("type", "text/javascript");
-	        fileref.setAttribute("src", scriptsCollection[startIndex]);
-	        fileref.onload = function() {
-	            startIndex = startIndex + 1;
-	            loadJsFilesSequentially(scriptsCollection, startIndex, librariesLoadedCallback);
-	        };
-	
-	        document.getElementsByTagName("head")[0].appendChild(fileref);
-	        filesadded += "[" + scriptsCollection[startIndex] + "]";
-    	} else {
-    		startIndex = startIndex + 1;
-	        loadJsFilesSequentially(scriptsCollection, startIndex, librariesLoadedCallback);
-    	}
-    } else {
-        librariesLoadedCallback();
-    }
-}
-
-// An array of scripts you want to load in order
-var scriptLibrary = [];
-//list of files already added
-var filesadded = "";
-
-function displayJSON(canvas) {
-    var writer = new draw2d.io.json.Writer();
-    writer.marshal(canvas, function(json) {
-        $("#json").text(JSON.stringify(json, null, 2));
-    });
-}
-
-function createJSFilesLibrary() {
-    scriptLibrary.push("static/js2db/lib/jquery-1.10.2.min.js");
-    scriptLibrary.push("static/js2db/lib/jquery.autoresize.js");
-    scriptLibrary.push("static/js2db/lib/jquery-touch_punch.js");
-    scriptLibrary.push("static/js2db/lib/jquery.contextmenu.js");
-
-    scriptLibrary.push("static/js2db/lib/shifty.js");
-
-    scriptLibrary.push("static/js2db/lib/raphael.js");
-    scriptLibrary.push("static/js2db/lib/rgbcolor.js");
-    scriptLibrary.push("static/js2db/lib/canvg.js");
-
-    scriptLibrary.push("static/js2db/lib/Class.js");
-
-    scriptLibrary.push("static/js2db/lib/json2.js");
-
-    scriptLibrary.push("static/js2db/lib/pathfinding-browser.min.js");
-	
-	scriptLibrary.push("static/js2db/draw2d/src/draw2d-all.js");
-	
-    scriptLibrary.push("static/js2db/lib/jquery.browser.js");
-    scriptLibrary.push("static/js2db/lib/jquery-ui-1.8.23.custom.min.js");
-    scriptLibrary.push("static/js2db/lib/jquery.layout.js");
-
-    scriptLibrary.push("static/js2db/dbModel/View.js");
-    scriptLibrary.push("static/js2db/dbModel/locator/PortRightLocator.js");
-    scriptLibrary.push("static/js2db/dbModel/locator/PortLeftLocator.js");
-    scriptLibrary.push("static/js2db/dbModel/shape/DBTable.js");
-    scriptLibrary.push("static/js2db/dbModel/shape/ManhattanRightConnectionLocator.js");
-    scriptLibrary.push("static/js2db/dbModel/shape/ManhattanLeftConnectionLocator.js");
-    scriptLibrary.push("static/js2db/dbModel/shape/TableConnection.js");
-    scriptLibrary.push("static/js2db/dbModel/shape/CustomLabel.js");
-
-}
