07b5ba9b5633c47c5ca536a24099f32bf63c34db
miguelross <miguel.ross872@gmail.com>
2016-12-14 07:43:18 -0500

    Modified static_root in settings.py for protoBase

--- protoBase/settings.py
+++ protoBase/settings.py
@@ -95,7 +95,7 @@ TIME_ZONE = 'America/Montreal'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # ======================   DEPLOY 
